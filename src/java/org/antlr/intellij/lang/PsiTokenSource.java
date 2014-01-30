3ab77f6d5c70a0487fbe6dbc29918adefc6178e6
Sam Harwell <sam@tunnelvisionlabs.com>
2014-01-30 11:33:44 -0600

    Simplified and generalized parsing stage

---
+++ src/java/org/antlr/intellij/lang/PsiTokenSource.java
@@ -0,0 +1,82 @@
+package org.antlr.intellij.lang;
+
+import com.intellij.lang.PsiBuilder;
+import org.antlr.intellij.lexer.TokenElementType;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CommonTokenFactory;
+import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.TokenFactory;
+import org.antlr.v4.runtime.TokenSource;
+import org.antlr.v4.runtime.misc.Pair;
+
+public class PsiTokenSource implements TokenSource {
+	protected PsiBuilder builder;
+	protected TokenFactory factory = CommonTokenFactory.DEFAULT;
+
+	public PsiTokenSource(PsiBuilder builder) {
+		this.builder = builder;
+	}
+
+	/* Colin: "the parsing lexer still has to return tokens that completely
+	 cover the file (i.e. no gaps). This is one of the most significant
+	 differences from a traditional compiler parser/lexer."
+
+	  after lots of trial and error I finally just put the BAD_TOKEN
+	  into the white space class so that they do not come to the parser
+	  but that IDEA still knows about them.
+	 */
+	@Override
+	public Token nextToken() {
+		TokenElementType ideaTType = (TokenElementType)builder.getTokenType();
+		int channel = Token.DEFAULT_CHANNEL;
+		int type;
+		if ( ideaTType==null ) {
+			type = Token.EOF;
+		}
+		else {
+			type = ideaTType.getType();
+		}
+
+		Pair<TokenSource, CharStream> source = new Pair<TokenSource, CharStream>(this, null);
+		String text = builder.getTokenText();
+		int start = builder.getCurrentOffset();
+		int length = text != null ? text.length() : 0;
+		int stop = start + length - 1;
+		// PsiBuilder doesn't provide line, column info
+		int line = 0;
+		int charPositionInLine = 0;
+		Token t = factory.create(source, type, text, channel, start, stop, line, charPositionInLine);
+		builder.advanceLexer();
+		return t;
+	}
+
+	@Override
+	public int getLine() {
+		return 0;
+	}
+
+	@Override
+	public int getCharPositionInLine() {
+		return 0;
+	}
+
+	@Override
+	public CharStream getInputStream() {
+		return null;
+	}
+
+	@Override
+	public String getSourceName() {
+		return "IDEA PsiBuilder";
+	}
+
+	@Override
+	public void setTokenFactory(@org.antlr.v4.runtime.misc.NotNull TokenFactory<?> factory) {
+		this.factory = factory;
+	}
+
+	@Override
+	public TokenFactory<?> getTokenFactory() {
+		return factory;
+	}
+}
