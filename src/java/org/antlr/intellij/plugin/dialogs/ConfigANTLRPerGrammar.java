746dd5bca598559c62b78a26dc10a3a9b49c08fc
Terence Parr <parrt@antlr.org>
2014-04-04 18:18:31 -0700

    toying with pref dialog

--- src/java/org/antlr/intellij/plugin/dialogs/ConfigANTLRPerGrammar.java
+++ src/java/org/antlr/intellij/plugin/dialogs/ConfigANTLRPerGrammar.java
@@ -9,7 +9,12 @@ import java.awt.*;
 
 public class ConfigANTLRPerGrammar extends DialogWrapper {
 	private JPanel dialogContents;
-	private JLabel labeltest;
+	private JTextField textField1;
+	private JTextField textField2;
+	private JTextField textField3;
+	private JCheckBox checkBox1;
+	private JCheckBox checkBox2;
+	private JTable argsTable;
 
 	public ConfigANTLRPerGrammar(Project project) {
 		super(project, false);
@@ -24,5 +29,15 @@ public class ConfigANTLRPerGrammar extends DialogWrapper {
 	@Override
 	protected JComponent createCenterPanel() {
 		return dialogContents;
+	}
+
+	private void createUIComponents() {
+		// TODO: place custom component creation code here
+		String[] columnNames = {"First Name", "Last Name"};
+		Object[][] data = {{"Kathy", "Smith"},{"John", "Doe"}};
+		argsTable = new JTable(data, columnNames);
+		if ( argsTable!=null ) {
+			argsTable.add(new JButton("flflf"));
+		}
 	}
 }
