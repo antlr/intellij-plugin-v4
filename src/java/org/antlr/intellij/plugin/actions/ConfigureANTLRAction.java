e45a8e9594cabad416c034b118971b747b284044
Terence Parr <parrt@antlr.org>
2014-04-05 17:12:49 -0700

    got per file antlr config working and persisting. Fixes #60.

--- src/java/org/antlr/intellij/plugin/actions/ConfigureANTLRAction.java
+++ src/java/org/antlr/intellij/plugin/actions/ConfigureANTLRAction.java
@@ -5,6 +5,7 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.LangDataKeys;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.openapi.vfs.VirtualFile;
 import org.antlr.intellij.plugin.dialogs.ConfigANTLRPerGrammar;
 
@@ -22,30 +23,15 @@ public class ConfigureANTLRAction extends AnAction implements DumbAware {
 		VirtualFile[] files = LangDataKeys.VIRTUAL_FILE_ARRAY.getData(e.getDataContext());
 		if ( files==null || files[0]==null ) return; // no files?
 
-		ConfigANTLRPerGrammar configDialog = new ConfigANTLRPerGrammar(project);
-		configDialog.getPeer().setTitle("Configure ANTLR for "+files[0].getName());
+		String name = files[0].getPath();
+		ConfigANTLRPerGrammar configDialog = new ConfigANTLRPerGrammar(project, name);
+		String fileName = files[0].getName();
+		configDialog.getPeer().setTitle("Configure ANTLR for "+ fileName);
 
 		configDialog.show();
 
-/*
-		Project project = getEventProject(e);
-		if ( project==null ) return; // whoa!
-		VirtualFile[] files = LangDataKeys.VIRTUAL_FILE_ARRAY.getData(e.getDataContext());
-		if ( files==null ) return; // no files?
-		String title = "ANTLR Code Generation";
-		boolean canBeCancelled = true;
-
-		// commit changes to PSI and file system
-		PsiDocumentManager.getInstance(project).commitAllDocuments();
-		FileDocumentManager.getInstance().saveAllDocuments();
-
-		Task.Backgroundable gen =
-			new RunANTLROnGrammarFile(files,
-									  project,
-									  title,
-									  canBeCancelled,
-									  new BackgroundFromStartOption());
-		ProgressManager.getInstance().run(gen);
-*/
+		if ( configDialog.getExitCode()==DialogWrapper.OK_EXIT_CODE ) {
+			configDialog.saveValues(project, name);
+		}
 	}
 }
