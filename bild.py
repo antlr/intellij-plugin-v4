1b0a68d0749a0f909b3044e184e785b0e2a328dd
Terence Parr <parrt@antlr.org>
2014-07-09 16:47:53 -0700

    refactor and use 4.4 to gen parsers.

--- bild.py
+++ bild.py
@@ -2,7 +2,6 @@
 
 # bootstrap by downloading bilder.py if not found
 import urllib
-import os
 
 if not os.path.exists("bilder.py"):
 	print "bootstrapping; downloading bilder.py"
@@ -13,17 +12,23 @@ if not os.path.exists("bilder.py"):
 # assumes bilder.py is in current directory
 from bilder import *
 
+def latest_antlr4():
+	# make sure we have latest antlr4 build
+	python("bild.py", workingdir="../antlr4", args="mkjar")
+	mkdir("lib")
+	# grab the lib that the plugin needs
+	copyfile(src="../antlr4/dist/antlr-4.4-complete.jar", trg="lib")
+	# put it in JARCARCHE too so bild can find it during antlr4()
+	copyfile(src="../antlr4/dist/antlr-4.4-complete.jar", trg=JARCACHE)
+
 def parsers():
-	antlr4("src/grammars", "gen", package="org.antlr.intellij.plugin.parser")
+	require(latest_antlr4)
+	antlr4("src/grammars", "gen", version="4.4", package="org.antlr.intellij.plugin.parser")
 
 def clean():
 	rmdir("gen")
 
 def all():
-	# make sure we have latest antlr4 build
-	python("bild.py", workingdir="../antlr4", args="mkjar")
-	mkdir("lib")
-	# grab the lib plugin needs
-	copyfile(src="../antlr4/dist/antlr-4.4-complete.jar", trg="lib")
+	require(parsers)
 
 processargs(globals())
