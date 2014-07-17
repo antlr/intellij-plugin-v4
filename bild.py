78d7e7ddbdb4833b67991ebf6cc030e0da523888
Terence Parr <parrt@antlr.org>
2014-07-16 17:49:47 -0700

    update bild for 4.4 antlr

--- bild.py
+++ bild.py
@@ -5,31 +5,33 @@ import urllib
 import os
 
 if not os.path.exists("bilder.py"):
-	print "bootstrapping; downloading bilder.py"
-	urllib.urlretrieve(
-		"https://raw.githubusercontent.com/parrt/bild/master/src/python/bilder.py",
-		"bilder.py")
+    print "bootstrapping; downloading bilder.py"
+    urllib.urlretrieve(
+        "https://raw.githubusercontent.com/parrt/bild/master/src/python/bilder.py",
+        "bilder.py")
 
 # assumes bilder.py is in current directory
 from bilder import *
 
+
 def latest_antlr4():
-	# make sure we have latest antlr4 build
-	python("bild.py", workingdir="../antlr4", args="mkjar")
-	mkdir("lib")
-	# grab the lib that the plugin needs
-	copyfile(src="../antlr4/dist/antlr-4.4-complete.jar", trg="lib")
-	# put it in JARCARCHE too so bild can find it during antlr4()
-	copyfile(src="../antlr4/dist/antlr-4.4-complete.jar", trg=JARCACHE)
+    mkdir("lib")
+    # grab the lib that the plugin needs
+    jarname = "antlr-4.4-complete.jar"
+    download("http://www.antlr.org/download/" + jarname, "lib")
 
 def parsers():
-	require(latest_antlr4)
-	antlr4("src/grammars", "gen", version="4.4", package="org.antlr.intellij.plugin.parser")
+    require(latest_antlr4)
+    antlr4("src/grammars", "gen", version="4.4",
+           package="org.antlr.intellij.plugin.parser")
+
 
 def clean():
-	rmdir("gen")
+    rmdir("gen")
+
 
 def all():
-	require(parsers)
+    require(parsers)
+
 
 processargs(globals())
