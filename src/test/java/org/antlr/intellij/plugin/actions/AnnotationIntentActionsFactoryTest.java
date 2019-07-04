37d906a845df787b6d5fbb162049826caffdb292
wojciszek <5840392+wojciszek@users.noreply.github.com>
2019-07-05 00:39:06 +0200

    Introduce AnnotationIntentActionsFactory

---
+++ src/test/java/org/antlr/intellij/plugin/actions/AnnotationIntentActionsFactoryTest.java
@@ -0,0 +1,26 @@
+package org.antlr.intellij.plugin.actions;
+
+import com.intellij.codeInsight.intention.IntentionAction;
+import com.intellij.openapi.util.TextRange;
+import org.antlr.intellij.plugin.psi.LexerRuleRefNode;
+import org.antlr.v4.tool.ErrorType;
+import org.junit.Test;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertFalse;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class AnnotationIntentActionsFactoryTest {
+    @Test
+    public void shouldReturnEmptyOptionalForUnsupportedErrorType() {
+        // when:
+        LexerRuleRefNode lexerRuleRefNode = mock(LexerRuleRefNode.class);
+        when(lexerRuleRefNode.isValid()).thenReturn(true);
+        Optional<IntentionAction> quickFix = AnnotationIntentActionsFactory.getFix(new TextRange(0, 1), ErrorType.INTERNAL_ERROR);
+
+        // then:
+        assertFalse(quickFix.isPresent());
+    }
+}
