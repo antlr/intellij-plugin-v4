07456b8929e76fdefc292de416c61eab973c1ca6
Bastien Jansen <bastien.jansen@gmx.com>
2023-03-30 14:08:29 +0200

    fix: make tests and CI work again

--- src/test/java/org/antlr/intellij/plugin/TestUtils.java
+++ src/test/java/org/antlr/intellij/plugin/TestUtils.java
@@ -1,5 +1,7 @@
 package org.antlr.intellij.plugin;
 
+import com.intellij.openapi.diagnostic.Attachment;
+import com.intellij.openapi.diagnostic.ExceptionWithAttachments;
 import com.intellij.util.ThrowableRunnable;
 import com.intellij.util.lang.CompoundRuntimeException;
 
@@ -13,20 +15,49 @@ public class TestUtils {
 		} catch (RuntimeException e) {
 			// We don't want to release the editor in the Tool Output tool window, so we ignore
 			// ObjectNotDisposedExceptions related to this particular editor
-			if ( e.getClass().getName().equals("com.intellij.openapi.util.TraceableDisposable$ObjectNotDisposedException")
-					|| e instanceof CompoundRuntimeException ) {
-				StringWriter stringWriter = new StringWriter();
-				e.printStackTrace(new PrintWriter(stringWriter));
-				String stack = stringWriter.toString();
-				if ( stack.contains("ANTLRv4PluginController.createToolWindows") ||
-						stack.contains("Issue559Test") || stack.contains("Issue540Test") ||
-						stack.contains("org.antlr.intellij.plugin.preview.InputPanel.createPreviewEditor") ) {
-					return;
-				}
+			if (exceptionShouldBeIgnored(e)) {
+				return;
 			}
 
 			throw e;
 		}
+	}
+
+	private static boolean exceptionShouldBeIgnored(RuntimeException e) {
+		if (e instanceof CompoundRuntimeException) {
+			for (Throwable exception : ((CompoundRuntimeException) e).getExceptions()) {
+				if (exception instanceof RuntimeException && exceptionShouldBeIgnored((RuntimeException) exception)) {
+					return true;
+				}
+			}
+		}
+
+		if (e.getClass().getName().equals("com.intellij.openapi.util.TraceableDisposable$ObjectNotDisposedException")) {
+			StringWriter stringWriter = new StringWriter();
+			e.printStackTrace(new PrintWriter(stringWriter));
+			String stack = stringWriter.toString();
+
+			if (stackMatchesOurEditorCreation(stack)) {
+				return true;
+			}
+		}
+
+		if (e.getClass().getName().equals("com.intellij.openapi.util.TraceableDisposable$DisposalException")) {
+			for (Attachment attachment : ((ExceptionWithAttachments) e).getAttachments()) {
+				if (stackMatchesOurEditorCreation(attachment.getDisplayText())) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+	private static boolean stackMatchesOurEditorCreation(String stack) {
+		return stack.contains("ANTLRv4PluginController.createToolWindows")
+			|| stack.contains("Issue559Test")
+			|| stack.contains("Issue540Test")
+			|| stack.contains("org.antlr.intellij.plugin.preview.InputPanel.createPreviewEditor");
 	}
 }
 
