f602647bba55f869f3287a83024701eb89fc42ed
Bill Fahle <wfahle@phasechange.ai>
2023-03-30 16:12:27 -0500

    Removed additional unused elements from GrammarElementRefNode
    Signed-off-by: Bill Fahle <wfahle@phasechange.ai>

--- src/main/java/org/antlr/intellij/plugin/psi/GrammarElementRefNode.java
+++ src/main/java/org/antlr/intellij/plugin/psi/GrammarElementRefNode.java
@@ -7,15 +7,12 @@ import com.intellij.psi.tree.IElementType;
  * Refs to tokens, rules
  */
 public abstract class GrammarElementRefNode extends LeafPsiElement {
-	protected String name = null; // an override to input text ID if we rename via intellij
-
 	public GrammarElementRefNode(IElementType type, CharSequence text) {
 		super(type, text);
 	}
 
 	@Override
 	public String getName() {
-		if ( name!=null ) return name;
 		return getText();
 	}
 
