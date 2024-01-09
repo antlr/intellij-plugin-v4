a67e6db0f5e14ffc4348960a73ca4bea623d9d7c
Bastien Jansen <bastien.jansen@gmx.com>
2024-01-09 08:55:21 +0100

    fix: handle cases when our lexer is instantiated before the ParserDefinition (fixes #681)

--- src/main/java/org/antlr/intellij/plugin/adaptors/ANTLRv4LexerAdaptor.java
+++ src/main/java/org/antlr/intellij/plugin/adaptors/ANTLRv4LexerAdaptor.java
@@ -1,12 +1,24 @@
 package org.antlr.intellij.plugin.adaptors;
 
 import org.antlr.intellij.adaptor.lexer.ANTLRLexerAdaptor;
+import org.antlr.intellij.adaptor.lexer.PSIElementTypeFactory;
 import org.antlr.intellij.plugin.ANTLRv4Language;
 import org.antlr.intellij.plugin.parser.ANTLRv4Lexer;
+import org.antlr.intellij.plugin.parser.ANTLRv4Parser;
 import org.antlr.v4.runtime.Lexer;
 
 /** Adapt ANTLR needs to intellij */
 public class ANTLRv4LexerAdaptor extends ANTLRLexerAdaptor {
+
+	// In case a lexer was created outside our ParserDefinition
+	static {
+		PSIElementTypeFactory.defineLanguageIElementTypes(
+				ANTLRv4Language.INSTANCE,
+				ANTLRv4Lexer.tokenNames,
+				ANTLRv4Parser.ruleNames
+		);
+	}
+
 	private static final ANTLRv4LexerState INITIAL_STATE = new ANTLRv4LexerState(Lexer.DEFAULT_MODE, null, 0);
 
 	public ANTLRv4LexerAdaptor(ANTLRv4Lexer lexer) {
